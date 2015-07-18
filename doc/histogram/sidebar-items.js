d33eb013b48cdcac26a5f11e1045e64e83e16a83
brayniac <brayniac@gmail.com>
2015-07-18 12:28:12 -0700

    histogram 0.1.2 rustdoc

---
+++ doc/histogram/sidebar-items.js
@@ -0,0 +1,1 @@
+initSidebarItems({"struct":[["Histogram",""],["HistogramConfig",""],["HistogramCounters",""],["HistogramData",""],["HistogramProperties",""]]});
---
+++ doc/implementors/core/default/trait.Default.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors['histogram'] = ["impl <a class='trait' href='http://doc.rust-lang.org/nightly/core/default/trait.Default.html' title='core::default::Default'>Default</a> for <a class='struct' href='histogram/struct.HistogramConfig.html' title='histogram::HistogramConfig'>HistogramConfig</a>","impl <a class='trait' href='http://doc.rust-lang.org/nightly/core/default/trait.Default.html' title='core::default::Default'>Default</a> for <a class='struct' href='histogram/struct.HistogramCounters.html' title='histogram::HistogramCounters'>HistogramCounters</a>",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
