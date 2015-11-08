e88e3483de75b2aa63490081e2eee36d503bc364
Brian Martin <brayniac@gmail.com>
2015-11-08 09:48:23 -0800

    histogram 0.3.0

--- doc/implementors/core/fmt/trait.Debug.js
+++ doc/implementors/core/fmt/trait.Debug.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {};
-implementors['histogram'] = ["impl <a class='trait' href='http://doc.rust-lang.org/nightly/core/fmt/trait.Debug.html' title='core::fmt::Debug'>Debug</a> for <a class='struct' href='histogram/struct.Histogram.html' title='histogram::Histogram'>Histogram</a>",];
+implementors['histogram'] = ["impl <a class='trait' href='https://doc.rust-lang.org/nightly/core/fmt/trait.Debug.html' title='core::fmt::Debug'>Debug</a> for <a class='struct' href='histogram/struct.Histogram.html' title='histogram::Histogram'>Histogram</a>",];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
